10a957d6275f6f4b30d4c3c66487a4cf19e3e6a5
kai <1145275516@qq.com>
2017-06-15 09:59:43 +0800

    kai

---
+++ app/src/main/java/com/example/yls/ad0302_json/Student.java
@@ -0,0 +1,43 @@
+package com.example.yls.ad0302_json;
+
+import java.util.ArrayList;
+
+
+public class Student {
+    int id;
+    String name;
+    int age;
+    ArrayList<Student> stuList;
+
+
+
+    public Student(int age, String name, int id) {
+        this.age = age;
+        this.name = name;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ app/src/test/java/com/example/yls/ad0302_json/ExampleUnitTest.java
@@ -0,0 +1,13 @@
+package com.example.yls.ad0302_json;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
